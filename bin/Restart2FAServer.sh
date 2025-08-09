b2beccbd7ff44b7b04f4803ee9e67f02a089c729
Mr BTC Gambler <105219812+mrbtcgambler@users.noreply.github.com>
2025-08-09 10:34:24 +0100

    Update Restart2FAServer.sh

--- bin/Restart2FAServer.sh
+++ bin/Restart2FAServer.sh
@@ -1,5 +1,5 @@
 cd ~/alphaverse-auto/
 screen -S 2faAPI -X quit
-screen -dmS 2faAPI && 2faServer -S 2faAPI -X stuff 'cd ~/alphaverse-auto && npm run 2faAPI\n'
+screen -dmS 2faAPI && 2faServer -S 2faAPI -X stuff 'cd ~/alphaverse-dev && npm run 2faAPI\n'
 sleep 1
 exit
