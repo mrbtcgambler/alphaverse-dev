6d37a988468356ee9ba64320b7932908e1e8e1c7
Mr BTC Gambler <105219812+mrbtcgambler@users.noreply.github.com>
2025-08-09 10:34:40 +0100

    Update RestartAPIServer.sh

--- bin/RestartAPIServer.sh
+++ bin/RestartAPIServer.sh
@@ -1,6 +1,6 @@
 cd ~/alphaverse-auto/
 screen -S apiserver -X quit
-screen -dmS apiserver && apiserver -S server -X stuff 'cd ~/alphaverse-auto && npm run apiServer\n'
+screen -dmS apiserver && apiserver -S server -X stuff 'cd ~/alphaverse-dev && npm run apiServer\n'
 sleep 1
 exit
 
